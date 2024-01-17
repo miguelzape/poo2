0fc3558ef9015ac1adc5ff08202232f71b95b807
miguel <inventado@gmail.com>
2024-01-17 18:49:53 +0100

    mas

--- codigoHTML/Ahorcado/codigo.js
+++ codigoHTML/Ahorcado/codigo.js
@@ -1,55 +1,65 @@
+let palabra;
+let aciertos = 0;
+let fallos = 0;
+let coincidencia = false;
 
-
-let palabra="abcde"
-
-function msg() {
-	// Comentario de una linea en javascript
-	palabra = "letra"
-	console.log(mensaje);
-	console.log(mensaje);
-	console.log("Paso1");
-	alert(mensaje);
-	prompt("Escriba una palabra")
-	/*
-	 * Comentarios de varias lineas, como ves es igual que en Java
-	 */
+function cogerPalabra() {
+	palabra = prompt("Escriba una palabra de 5 letras").toLowerCase();
+	reiniciar();
 }
 
+function probar() {
+	caracter = document.getElementById("letra").value;
+	anterior = aciertos;
+	coincidencia = false;
+	
+	for (var i=0;i<5;i++){
+		probarSimple(caracter, i, "r"+(i+1));	
+	}
+//	probarSimple(caracter, 0, "r1");
+//	probarSimple(caracter, 1, "r2");
+//	probarSimple(caracter, 2, "r3");
+//	probarSimple(caracter, 3, "r4");
+//	probarSimple(caracter, 4, "r5");
 
-
-
-function sum(num1, num2) {
-	console.log("Numero1: " + num1);
-	console.log("Numero2: " + num2);
-	return num1 + num2;
+	if (!coincidencia) {
+		fallos = fallos + 1;
+		intentos = 5 - fallos;
+		document.getElementById('mensaje2').innerHTML = intentos;
+	}
+	if (fallos >= 5) {
+		document.getElementById("mensaje1").innerHTML = "HAS PERDIDO";
+	}
+	else if (aciertos >= 5) {
+		document.getElementById("mensaje1").innerHTML = "HAS GANADO";
+	}
+	caracter = document.getElementById("letra").value="";
+	caracter = document.getElementById("letra").focus();
 }
 
-function mensajeTrueFalse() {
-	var respuesta = confirm("Estas seguro?");
-	if (respuesta == true) {
-		alert("ok");
-	} else {
-		alert("cancel");
+function probarSimple(caracter, posicion, controlName) {
+	//console.log(caracter);
+	if (document.getElementById(controlName).value != caracter) {
+		if (palabra.charAt(posicion) == caracter) {
+			aciertos = aciertos + 1;
+			document.getElementById(controlName).value = caracter;
+			coincidencia = true;
+		}
+	}
+	else {
+		coincidencia = true;
 	}
 }
 
-function mensajePregunta() {
-	var nombre = prompt("¿Cuál es tu nombre?");
-	alert("Mi nombre es " + nombre);
-}
-
-function pintaTexto(texto, numVeces) {
-	for (i=1; i<=numVeces; i++)  
-	{  
-		document.write(texto + "<br/>")  
-	}  
-}
-
-
-function pintaArray(){
-	var peliculas=["Matrix","Terminator","Blade Runner"];
-	for (i=1; i<=peliculas.length; i++)  
-	{  
-		document.write(peliculas[i] + "<br/>")  
-	} 
+function reiniciar() {
+	aciertos = 0;
+	fallos = 0;
+	document.getElementById("r1").value = "";
+	document.getElementById("r2").value = "";
+	document.getElementById("r3").value = "";
+	document.getElementById("r4").value = "";
+	document.getElementById("r5").value = "";
+	document.getElementById("letra").value = "";
+	document.getElementById("mensaje1").innerHTML = "BIENVENIDO";
+	document.getElementById('mensaje2').innerHTML = "5";
 }
